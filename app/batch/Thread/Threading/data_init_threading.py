748cb7e7e93024c9a671b600eb34de6cd08f4529
kalraid <kalraid@gmail.com>
2022-03-26 11:40:52 +0900

    [운영] ID3기 반영 - 스케쥴 0초 단위 -> 4시,16시반

--- app/batch/Thread/Threading/data_init_threading.py
+++ app/batch/Thread/Threading/data_init_threading.py
@@ -15,12 +15,11 @@ class DataInitThreading(threading.Thread):
         super().__init__()
 
     def run(self):
-        sched.add_job(job, 'cron', second='0', id="test_3")
+        sched.add_job(job, 'cron', hour='4,12', minute='30', id='test_2')
         print('sched before~')
         sched.start()
         print('sched after~')
 
 
-@sched.scheduled_job('cron', hour='12', minute='30', id='test_2')
 def job():
     init_data()
